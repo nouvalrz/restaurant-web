25c8dcab9a7c1c3fa757b330958a28e276b4751c
root <root@localhost.localdomain>
2024-11-01 02:08:40 +0000

    finish landing page

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,17 @@
 import 'regenerator-runtime'; /* for async await transpile */
 import '../styles/main.scss';
+import Home from './pages/home';
+
+const pages = {
+  "/": Home
+}
+
+const currentRoute = window.location.pathname;
+
+window.addEventListener("DOMContentLoaded", () => {
+  if(pages[currentRoute]){
+    pages[currentRoute].init();
+  }else{
+    window.location.href = "/";
+  }
+})
